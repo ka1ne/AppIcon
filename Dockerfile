8d2be7e516dbd2939c669883ce0254f6611a11ac
Nathan Cook <nathan@Nathans-MBP.client.easlab.co.uk>
2025-01-21 09:56:37 +0000

    use different image download handling

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ COPY Sources ./Sources
 COPY Tests ./Tests
 
 # Install system dependencies for the build
-RUN apt-get update && apt-get install -y libgd-dev libjpeg-dev libpng-dev make
+RUN apt-get update && apt-get install -y libgd-dev libjpeg-dev libpng-dev make imagemagick
 
 # Run Swift build and then install
 RUN make install
--- Sources/AppIconCore/Command/Command.swift
+++ Sources/AppIconCore/Command/Command.swift
@@ -11,7 +11,12 @@ enum Command {
         case .createDirectory(let output):
             try execute("/bin/mkdir", "-p", output)
         case .extractImage(let base, let output, let size):
-            try execute("/usr/bin/sips", "-Z", "\(size)", base, "--out", output)
+            #if os(macOS)
+                try execute("/usr/bin/sips", "-Z", "\(size)", base, "--out", output)
+            #else
+                // Use ImageMagick's convert command on Linux
+                try execute("/usr/bin/convert", base, "-resize", "\(Int(size))x\(Int(size))", output)
+            #endif
         case .createJSON(let json, let output):
             let writefile = try open(forWriting: output, overwrite: true)
             writefile.write(json)
@@ -20,9 +25,11 @@ enum Command {
     }
 
     private func execute(_ executable: String, _ args: String...) throws {
+        print("Executing: \(executable) \(args.joined(separator: " "))")
         let runOutput = run(executable, args)
 
-        guard runOutput.succeeded else {
+        if !runOutput.succeeded {
+            print("Command failed with error: \(runOutput.stderror)")
             throw LocalError.execution
         }
     }
